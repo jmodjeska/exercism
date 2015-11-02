952c8977aae66f139d5f836b7ec1df4b9f444eca
Jeremy Modjeska <jeremy@moz.com>
2015-11-02 00:57:48 -0800

    Nth Prime v2

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -3,7 +3,8 @@ class Prime
     raise ArgumentError.new("数太小") unless i > 0
     primes, t = [1, 2], 3
     until primes.size > i do
-      primes << t unless (2..t-1).any? { |x| t % x == 0 }
+      max = Math.sqrt(t).round
+      primes << t unless (2..max).any? { |x| t % x == 0 }
       t += 2
     end
     return primes[i]
